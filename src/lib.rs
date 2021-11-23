81beb5a4e8f7b154dfc19258a63f453f51a57147
Greg Kapka <greg@oraclize.it>
2021-11-23 00:02:24 +0000

    feat(mnemonics): <- adds mvp of those

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,13 @@
 //! A rust library for building on the Algorand blockchain.
 #[macro_use]
 extern crate quick_error;
+#[macro_use]
+extern crate lazy_static;
 
 mod crypto_utils;
 mod errors;
 mod keys;
+mod mnemonic;
 mod types;
 
 pub use crate::keys::AlgorandKeys;
