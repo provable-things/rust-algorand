b63743d0abca1afbf07ef867ff4019b346bfcedb
Greg Kapka <greg@oraclize.it>
2021-12-06 20:49:35 +0000

    feat(micro-algos): <- adds mod to handle those

---
+++ src/algorand_micro_algos.rs
@@ -0,0 +1,116 @@
+use std::{
+    cmp::Ordering,
+    fmt,
+    ops::{Add, Mul, Sub},
+};
+
+use serde::{Serialize, Serializer};
+
+use crate::types::Result;
+
+const ALGORAND_MINIMUM_FEE: u64 = 1_000;
+const MICRO_ALGOS_MULTIPLIER: u64 = 1_000_000;
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd)]
+pub struct MicroAlgos(pub(crate) u64);
+
+impl MicroAlgos {
+    fn from_algos(u_64: u64) -> Self {
+        Self(u_64 / MICRO_ALGOS_MULTIPLIER)
+    }
+
+    fn to_algos(&self) -> u64 {
+        self.0 * MICRO_ALGOS_MULTIPLIER
+    }
+
+    fn satisfies_minimum_fee(&self) -> bool {
+        self >= &Self::minimum_fee()
+    }
+
+    /// ## Minimum Fee
+    ///
+    /// Get the minimum fee for an Algorand transaction in MicroAlgos.
+    pub fn minimum_fee() -> Self {
+        Self(ALGORAND_MINIMUM_FEE)
+    }
+
+    /// ## Check if Satisfies Minimum Fee
+    ///
+    /// ## Check is an amount of MicroAlgos satisfies the minimum transaction fee required by the
+    /// Algorand protocol.
+    pub fn check_if_satisfies_minimum_fee(&self) -> Result<Self> {
+        if self.satisfies_minimum_fee() {
+            Ok(self.clone())
+        } else {
+            Err(format!(
+                "Fee is below the minimum algorand fee of {} MicroAlgos!",
+                ALGORAND_MINIMUM_FEE
+            )
+            .into())
+        }
+    }
+}
+
+impl fmt::Display for MicroAlgos {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.to_algos())
+    }
+}
+
+impl Add<u64> for MicroAlgos {
+    type Output = Self;
+
+    fn add(self, rhs: u64) -> Self::Output {
+        MicroAlgos(self.0 + rhs)
+    }
+}
+
+impl Sub<u64> for MicroAlgos {
+    type Output = Self;
+
+    fn sub(self, rhs: u64) -> Self::Output {
+        MicroAlgos(self.0 - rhs)
+    }
+}
+
+impl Mul<u64> for MicroAlgos {
+    type Output = Self;
+
+    fn mul(self, rhs: u64) -> Self::Output {
+        MicroAlgos(self.0 * rhs)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::errors::AppError;
+
+    #[test]
+    fn minimum_fee_should_be_correct() {
+        let result = MicroAlgos::minimum_fee().to_algos();
+        let expected_result = ALGORAND_MINIMUM_FEE * MICRO_ALGOS_MULTIPLIER;
+        assert_eq!(result, expected_result);
+    }
+
+    #[test]
+    fn should_satisfy_minimum_fee() {
+        let num = ALGORAND_MINIMUM_FEE * MICRO_ALGOS_MULTIPLIER;
+        let result = MicroAlgos(num).check_if_satisfies_minimum_fee();
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn should_fail_minimum_fee_check_if_amount_too_low() {
+        let num = (ALGORAND_MINIMUM_FEE * MICRO_ALGOS_MULTIPLIER) - 1;
+        let expected_error = format!(
+            "Fee is below the minimum algorand fee of {} MicroAlgos!",
+            ALGORAND_MINIMUM_FEE
+        );
+        match MicroAlgos::from_algos(num).check_if_satisfies_minimum_fee() {
+            Ok(_) => panic!("Should not have succeeded!"),
+            Err(AppError::Custom(error)) => assert_eq!(error, expected_error),
+            Err(_) => panic!("Wrong err received!"),
+        };
+    }
+}
--- src/constants.rs
+++ src/constants.rs
@@ -1,7 +1,6 @@
 /// ## Max Num Rounds
 ///
 /// The maximum number of rounds a transaction may be valid for.
-pub const MAX_NUM_ROUNDS: u64 = 1000;
-
+pub const ALGORAND_MAX_NUM_ROUNDS: u64 = 1000;
 pub const ALGORAND_MAINNET_GENESIS_ID: &str = "mainnet-v1.0";
 pub const ALGORAND_MAINNET_GENESIS_HASH: &str = "wGHE2Pwdvd7S12BL5FaOP20EGYesN73ktiC1qzkkit8=";
