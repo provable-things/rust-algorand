44e3133e623c432a4ac5c6780787744041a54ef4
Greg Kapka <greg@oraclize.it>
2021-12-07 19:32:11 +0000

    feat(transaction): <- adds tests to that

--- src/algorand_transaction.rs
+++ src/algorand_transaction.rs
@@ -11,13 +11,13 @@ use crate::{
     algorand_micro_algos::MicroAlgos,
     algorand_signature::AlgorandSignature,
     algorand_traits::ToMsgPackBytes,
+    algorand_types::{Byte, Bytes, Result},
     constants::ALGORAND_MAX_NUM_ROUNDS,
     crypto_utils::{base32_encode_with_no_padding, sha512_256_hash_bytes},
-    algorand_types::{Byte, Bytes, Result},
 };
 
-impl ToMsgPackBytes for AlgorandSignedTransaction {}
 impl ToMsgPackBytes for AlgorandTransaction {}
+impl ToMsgPackBytes for AlgorandSignedTransaction {}
 
 /// ## An Algorand Tx
 ///
@@ -129,7 +129,6 @@ pub struct AlgorandTransaction {
 }
 
 impl AlgorandTransaction {
-    // TODO test!
     fn calculate_last_valid_round(
         first_valid_round: u64,
         last_valid_round: Option<u64>,
@@ -138,9 +137,9 @@ impl AlgorandTransaction {
             None => Ok(first_valid_round + ALGORAND_MAX_NUM_ROUNDS),
             Some(last_valid_round_number) => {
                 if last_valid_round_number <= first_valid_round {
-                    Ok(last_valid_round_number)
-                } else {
                     Err("Last valid round must be > than first valid round!".into())
+                } else {
+                    Ok(last_valid_round_number)
                 }
             },
         }
@@ -253,7 +252,11 @@ impl AlgorandSignedTransaction {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{constants::ALGORAND_MAINNET_GENESIS_ID, test_utils::get_sample_algorand_keys};
+    use crate::{
+        constants::ALGORAND_MAINNET_GENESIS_ID,
+        errors::AppError,
+        test_utils::get_sample_algorand_keys,
+    };
 
     fn get_sample_tx() -> AlgorandTransaction {
         let first_valid_round = 1000;
@@ -306,5 +309,41 @@ mod tests {
         let result = hex::encode(signed_tx.to_msg_pack_bytes().unwrap());
         let expected_result = "82a3736967c440a6e1b839f2f0109afa914694264e1fcee2ce3b5858ff46c436a69ece9f9630d9d182b1307f4bb8a1807e785326f91beded1a6d1a2368c1e0644b58bf6bc60f06a374786e88a3616d74cd0539a3666565cd03e8a26676cd03e8a26768c420c061c4d8fc1dbdded2d7604be4568e3f6d041987ac37bde4b620b5ab39248adfa26c76cd07d0a3726376c4203516382099d0a8f0c8eabc6d4b7efb58b0be7008e98d239dd0199490674e0372a3736e64c420e23319d1dfd271db1f8752ea384948a975594617fa0b546d44740e768a14b899a474797065a3706179";
         assert_eq!(result, expected_result);
+    }
+
+    #[test]
+    fn should_calculate_last_valid_round_if_none_given() {
+        let first_valid_round = 1000;
+        let result =
+            AlgorandTransaction::calculate_last_valid_round(first_valid_round, None).unwrap();
+        let expected_result = 2000;
+        assert_eq!(result, expected_result);
+    }
+
+    #[test]
+    fn should_error_if_last_valid_round_lte_first_valid_round() {
+        let first_valid_round = 1000;
+        let last_valid_round = first_valid_round - 1;
+        let expected_error = "Last valid round must be > than first valid round!";
+        match AlgorandTransaction::calculate_last_valid_round(
+            first_valid_round,
+            Some(last_valid_round),
+        ) {
+            Ok(_) => panic!("Should not have succeeded!"),
+            Err(AppError::Custom(error)) => assert_eq!(error, expected_error),
+            Err(_) => panic!("Wrong error received"),
+        }
+    }
+
+    #[test]
+    fn should_use_last_valid_round_if_valid() {
+        let first_valid_round = 1000;
+        let last_valid_round = 1001;
+        let result = AlgorandTransaction::calculate_last_valid_round(
+            first_valid_round,
+            Some(last_valid_round),
+        )
+        .unwrap();
+        assert_eq!(result, last_valid_round);
     }
 }
