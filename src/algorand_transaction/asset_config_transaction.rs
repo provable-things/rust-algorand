313048bb4d8cb7dfc5c68e5f3faaeccff0db66ae
Greg Kapka <greg@oraclize.it>
2021-12-08 19:21:26 +0000

    feat(txs): <- adds asset config tx type

--- src/algorand_transaction/asset_config_transaction.rs
+++ src/algorand_transaction/asset_config_transaction.rs
@@ -2,12 +2,12 @@ use derive_more::Constructor;
 use serde::Serialize;
 
 use crate::{
-    algorand_hash::AlgorandHash,
     algorand_address::AlgorandAddress,
-    algorand_transaction::{
-        transaction_type::AlgorandTransactionType,
-
-    },
+    algorand_hash::AlgorandHash,
+    algorand_micro_algos::MicroAlgos,
+    algorand_transaction::{transaction_type::AlgorandTransactionType, AlgorandTransaction},
+    algorand_types::{Bytes, Result},
+    constants::ALGORAND_MAX_NUM_ROUNDS,
 };
 
 #[derive(Debug, Clone, Eq, PartialEq, Constructor, Serialize)]
@@ -41,4 +41,45 @@ pub struct AssetParameters {
 
     #[serde(rename(serialize = "un"))]
     unit_name: String,
+}
+
+impl AlgorandTransaction {
+    fn new_asset_configuration_tx(
+        asset_parameters: AssetParameters,
+        amount: u64, // FIXME check this is above 100,000 minimum!
+        fee: MicroAlgos,
+        note: Option<Bytes>,
+        first_valid_round: u64,
+        sender: AlgorandAddress,
+        receiver: AlgorandAddress,
+        genesis_hash: AlgorandHash,
+        last_valid_round: Option<u64>,
+    ) -> Result<Self> {
+        let calculated_last_valid_round =
+            Self::calculate_last_valid_round(first_valid_round, last_valid_round)?;
+        if calculated_last_valid_round > first_valid_round + ALGORAND_MAX_NUM_ROUNDS {
+            return Err(format!(
+                "Last valid round of {} is > {} away from first valid round of {}!",
+                calculated_last_valid_round, ALGORAND_MAX_NUM_ROUNDS, first_valid_round
+            )
+            .into());
+        };
+        Ok(Self {
+            note,
+            sender,
+            genesis_hash,
+            first_valid_round,
+            amount: Some(amount),
+            receiver: Some(receiver),
+            asset_parameters: Some(asset_parameters),
+            fee: fee.check_if_satisfies_minimum_fee()?.0,
+            last_valid_round: calculated_last_valid_round,
+            txn_type: AlgorandTransactionType::AssetConfiguration,
+            group: None,
+            lease: None,
+            rekey_to: None,
+            genesis_id: None,
+            close_remainder_to: None,
+        })
+    }
 }
