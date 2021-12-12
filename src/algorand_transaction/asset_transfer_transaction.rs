a16b0f89bb80dc95a08960cd56bba11c33e7dc59
Greg Kapka <greg@oraclize.it>
2021-12-12 17:05:10 +0000

    feat(asset-txs): <- adds test for those

--- src/algorand_transaction/asset_transfer_transaction.rs
+++ src/algorand_transaction/asset_transfer_transaction.rs
@@ -115,4 +115,28 @@ mod tests {
         let expected_result = "82a3736967c4404e714fa39e98ed14a1220a9fe25efbba3b47ccbf784b28bdff0c5fa1f71d1d289034f1157b97ceb63c0cedb3a6790ba87930f6a7a40d495d41ecde101597cd09a374786e88a461726376c42090826960db089ee5636266600d56a9f41f5d037e5c90a18007e384fc1558603da3666565cd03e8a26676ce01121536a26768c420c061c4d8fc1dbdded2d7604be4568e3f6d041987ac37bde4b620b5ab39248adfa26c76ce0112191ea3736e64c42090826960db089ee5636266600d56a9f41f5d037e5c90a18007e384fc1558603da474797065a56178666572a478616964ce1b9cddb0";
         assert_eq!(result, expected_result);
     }
+
+    #[test]
+    fn should_sign_asset_transfer_tx() {
+        let tx = AlgorandTransaction::asset_transfer(
+            463265200,
+            MicroAlgos(1000),
+            100001337,
+            17_962_505,
+            AlgorandAddress::from_str("GSKWPLI7YL7OF23F5ET5L7HSFLLJL3F5DUO7AH2HQLOSO4DRRHR76TDQ2I")
+                .unwrap(),
+            AlgorandHash::mainnet_genesis_hash().unwrap(),
+            None,
+            AlgorandAddress::from_str("GKT5XX6N45UV3ENMIOAVF7EQQYL77P45XFHYIPBFAJUON7RBUCQPX572TI")
+                .unwrap(),
+        )
+        .unwrap();
+        let result = tx
+            .sign(&get_sample_algorand_keys())
+            .unwrap()
+            .to_hex()
+            .unwrap();
+        let expected_result = "82a3736967c440df49d27ce4b5436e7bee58ffcdbe7ff2fb87b56c96309c997f9b525dcac33a78df3992c0ab4ce92b5da3bd7933b7318fdd15e92043bc6d120047e108e437ab07a374786e89a461616d74ce05f5e639a461726376c42032a7dbdfcde7695d91ac438152fc908617ffbf9db94f843c250268e6fe21a0a0a3666565cd03e8a26676ce01121609a26768c420c061c4d8fc1dbdded2d7604be4568e3f6d041987ac37bde4b620b5ab39248adfa26c76ce011219f1a3736e64c420349567ad1fc2fee2eb65e927d5fcf22ad695ecbd1d1df01f4782dd27707189e3a474797065a56178666572a478616964ce1b9cddb0";
+        assert_eq!(result, expected_result);
+    }
 }
