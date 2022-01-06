f4dfa67979cffaba586c21d8ff68a329906d712b
Greg Kapka <greg@oraclize.it>
2022-01-06 18:25:56 +0000

    feat(upgrade-vote): <- adds JSON parser to that

--- src/algorand_blocks/upgrade_vote.rs
+++ src/algorand_blocks/upgrade_vote.rs
@@ -7,6 +7,32 @@ use crate::algorand_types::Result;
 #[skip_serializing_none]
 #[derive(Debug, Default, Serialize, Deserialize)]
 pub struct UpgradeVote {
+    #[serde(rename = "upgradeprop")]
+    upgrade_propose: Option<String>,
+
+    #[serde(rename = "upgradedelay")]
+    upgrade_delay: Option<u64>,
+
+    #[serde(rename = "upgradeyes")]
+    upgrade_approve: Option<bool>,
+}
+
+impl UpgradeVote {
+    fn from_json(json: UpgradeVoteJson) -> Self {
+        Self {
+            upgrade_delay: json.upgrade_delay,
+            upgrade_propose: json.upgrade_propose,
+            upgrade_approve: json.upgrade_approve,
+        }
+    }
+
+    pub fn from_str(s: &str) -> Result<Self> {
+        UpgradeVoteJson::from_str(s).map(Self::from_json)
+    }
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+pub struct UpgradeVoteJson {
     #[serde(rename = "upgrade-propose")]
     upgrade_propose: Option<String>,
 
@@ -17,7 +43,7 @@ pub struct UpgradeVote {
     upgrade_approve: Option<bool>,
 }
 
-impl UpgradeVote {
+impl UpgradeVoteJson {
     pub fn from_str(s: &str) -> Result<Self> {
         Ok(serde_json::from_str(s)?)
     }
