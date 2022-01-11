ed1a73f44d2793feab6bc7ac742c9bcce00df74e
Greg Kapka <greg@oraclize.it>
2022-01-11 18:24:54 +0000

    feat(tx-json): <- adds mod to parse those

--- src/algorand_transactions/transaction_json.rs
+++ src/algorand_transactions/transaction_json.rs
@@ -3,13 +3,70 @@ use std::str::FromStr;
 use serde::{Deserialize, Serialize};
 use serde_with::skip_serializing_none;
 
-use crate::{algorand_types::Result, errors::AppError};
+use crate::{
+    algorand_transactions::asset_config_transaction::AssetConfigTransactionJson,
+    algorand_types::Result,
+    errors::AppError,
+};
 
 #[skip_serializing_none]
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
 pub struct AlgorandTransactionJson {
+    pub sender: String,
+
+    pub fee: u64,
+
+    #[serde(rename = "first-valid")]
+    pub first_valid: u64,
+
+    #[serde(rename = "last-valid")]
+    pub last_valid: u64,
+
+    pub note: Option<String>,
+
+    #[serde(rename = "genesis-id")]
+    pub genesis_id: Option<String>,
+
+    #[serde(rename = "genesis-hash")]
+    pub genesis_hash: String,
+
     #[serde(rename = "tx-type")]
-    tx_type: String,
+    pub tx_type: String,
+
+    /// Specifies if this tx is part of a group tx, and if so is the digest of that group.
+    pub group: Option<String>,
+
+    /// 32 bytes identifier. While this transaction possesses the lease, no other transaction
+    /// specifying this lease can be confirmed.
+    pub lease: Option<String>,
+
+    #[serde(rename = "rekey-to")]
+    pub rekey_to: Option<String>,
+
+    #[serde(rename = "asset-config-transaction")]
+    pub asset_config_transaction: Option<AssetConfigTransactionJson>,
+
+    #[serde(rename = "asset-sender")]
+    pub asset_sender: Option<String>,
+
+    #[serde(rename = "asset-receiver")]
+    pub asset_receiver: Option<String>,
+
+    pub receiver: Option<String>,
+
+    #[serde(rename = "close-remainder-to")]
+    pub close_remainder_to: Option<String>,
+
+    #[serde(rename = "asset-amount")]
+    pub asset_amount: Option<u64>,
+
+    #[serde(rename = "transfer-asset-id")]
+    pub transfer_asset_id: Option<u64>,
+
+    #[serde(rename = "asset-id")]
+    pub asset_id: Option<u64>,
+
+    pub amount: Option<u64>,
 }
 
 impl FromStr for AlgorandTransactionJson {
@@ -29,7 +86,11 @@ mod tests {
     fn should_get_txs_from_strs() {
         let txs = get_sample_txs_json_strs_n(0);
         txs.iter().for_each(|tx_json_str| {
-            AlgorandTransactionJson::from_str(tx_json_str).unwrap();
+            let result = AlgorandTransactionJson::from_str(tx_json_str);
+            if result.is_err() {
+                println!("{}", tx_json_str);
+                result.unwrap();
+            }
         });
     }
 }
