0ccdd081979526abe8dc084f4fa60b9d89b56acb
Greg Kapka <greg@oraclize.it>
2022-01-11 18:25:23 +0000

    feat(tx-types): <- impls `FromStr` on those

--- src/algorand_transactions/transaction_type.rs
+++ src/algorand_transactions/transaction_type.rs
@@ -1,11 +1,14 @@
-use std::fmt;
+use std::{fmt, str::FromStr};
 
 use serde::{Deserialize, Serialize, Serializer};
+
+use crate::{algorand_types::Result, errors::AppError};
 
 #[derive(Debug, Clone, PartialEq, Eq, Deserialize)]
 pub enum AlgorandTransactionType {
     Pay,
     AssetTransfer,
+    ApplicationCall,
     AssetConfiguration,
 }
 
@@ -14,6 +17,7 @@ impl fmt::Display for AlgorandTransactionType {
         match self {
             Self::Pay => write!(f, "pay"),
             Self::AssetTransfer => write!(f, "axfer"),
+            Self::ApplicationCall => write!(f, "appl"),
             Self::AssetConfiguration => write!(f, "acfg"),
         }
     }
@@ -28,5 +32,19 @@ impl Serialize for AlgorandTransactionType {
         S: Serializer,
     {
         serializer.serialize_str(&format!("{}", self))
+    }
+}
+
+impl FromStr for AlgorandTransactionType {
+    type Err = AppError;
+
+    fn from_str(s: &str) -> Result<Self> {
+        match s {
+            "pay" => Ok(Self::Pay),
+            "axfer" => Ok(Self::AssetTransfer),
+            "appl" => Ok(Self::ApplicationCall),
+            "acfg" => Ok(Self::AssetConfiguration),
+            _ => Err(format!("Unrecognized Algorand tx type: '{}'!", s).into()),
+        }
     }
 }
