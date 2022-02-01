cd51defab4982202ae86634e01c10cc8c88fb441
Greg Kapka <greg@oraclize.it>
2022-02-01 10:44:00 +0000

    feat(encoding): <- use macro to generate byte array visitors

--- src/algorand_encoding.rs
+++ src/algorand_encoding.rs
@@ -1,21 +1,33 @@
+use paste::paste;
 use serde::{de::Visitor, Deserialize};
 
-pub struct U8_32Visitor;
+macro_rules! make_byte_array_visitors {
+    ($($num:expr),*) => {
+        paste! {
+            $(
+                pub struct [<U8_ $num Visitor>];
 
-impl<'de> Visitor<'de> for U8_32Visitor {
-    type Value = [u8; 32];
+                impl<'de> Visitor<'de> for [<U8_ $num Visitor>] {
+                    type Value = [u8; $num];
 
-    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-        formatter.write_str("a 32 byte array")
-    }
+                    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                        let s = format!("a {} byte array", $num);
+                        formatter.write_str(&s)
+                    }
 
-    fn visit_bytes<E: serde::de::Error>(self, v: &[u8]) -> std::result::Result<Self::Value, E> {
-        if v.len() == 32 {
-            let mut bytes = [0; 32];
-            bytes.copy_from_slice(v);
-            Ok(bytes)
-        } else {
-            Err(E::custom(format!("Invalid byte array length: {}", v.len())))
+                    fn visit_bytes<E: serde::de::Error>(self, v: &[u8]) -> std::result::Result<Self::Value, E> {
+                        if v.len() == $num {
+                            let mut bytes = [0; $num];
+                            bytes.copy_from_slice(v);
+                            Ok(bytes)
+                        } else {
+                            Err(E::custom(format!("Invalid byte array length: {}", v.len())))
+                        }
+                    }
+                }
+            )*
         }
     }
 }
+
+make_byte_array_visitors!(32, 64);
