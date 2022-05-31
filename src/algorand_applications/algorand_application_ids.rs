b0ec43d596b0e42ed48f507d719431673e70e284
Greg Kapka <greg@oraclize.it>
2022-05-31 16:38:46 +0100

    feat(app-ids): <- impl `ToApplicationArg` on that & test

--- src/algorand_applications/algorand_application_ids.rs
+++ src/algorand_applications/algorand_application_ids.rs
@@ -1,12 +1,29 @@
+use derive_more::{Constructor, Deref};
+
 use crate::{
     algorand_applications::algorand_application_args::AlgorandApplicationArg,
     algorand_traits::ToApplicationArg,
 };
 
+#[derive(Clone, Debug, Default, Constructor, Deref)]
 pub struct AlgorandAppId(pub i32);
 
 impl ToApplicationArg for AlgorandAppId {
     fn to_application_arg(&self) -> AlgorandApplicationArg {
-        unimplemented!();
+        AlgorandApplicationArg(self.to_be_bytes().to_vec())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::algorand_types::Bytes;
+
+    #[test]
+    fn should_convert_algorand_app_id_to_app_arg() {
+        let app_id = AlgorandAppId::new(1337);
+        let result = app_id.to_application_arg().to_hex();
+        let expected_result = "00000539";
+        assert_eq!(result, expected_result);
     }
 }
