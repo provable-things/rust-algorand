9f25c469940e0ee1975c5be16bc41900fbb9ac09
Greg Kapka <greg@oraclize.it>
2022-05-31 18:41:27 +0100

    feat(app-args): <- impl `From` for `AlgorandAddress` in those

--- src/algorand_applications/algorand_application_args.rs
+++ src/algorand_applications/algorand_application_args.rs
@@ -1,7 +1,11 @@
 use derive_more::{Constructor, Deref};
 use serde::{Deserialize, Serialize};
 
-use crate::algorand_types::Bytes;
+use crate::{
+    algorand_address::AlgorandAddress,
+    algorand_traits::ToApplicationArg,
+    algorand_types::Bytes,
+};
 
 #[derive(Default, Debug, Eq, PartialEq, Clone, Serialize, Deserialize, Constructor, Deref)]
 pub struct AlgorandApplicationArg(#[serde(with = "serde_bytes")] pub Bytes);
@@ -39,5 +43,11 @@ impl From<i64> for AlgorandApplicationArg {
 impl From<u64> for AlgorandApplicationArg {
     fn from(n: u64) -> Self {
         Self::new(n.to_be_bytes().to_vec())
+    }
+}
+
+impl From<AlgorandAddress> for AlgorandApplicationArg {
+    fn from(address: AlgorandAddress) -> Self {
+        address.to_application_arg()
     }
 }
